f11937ac6f1cc4d112eadcc9f6b50e4a0e868503
Aaron Peyton <thealphabet101@gmail.co>
2017-09-28 08:53:53 -0700

    added sq function and test

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,25 +1,30 @@
 import math
 import unittest
 
-def circleArea(radius):
-	return 1;
+def circleArea(radius): #no return type nor parameter type
+    return radius**2.0 * math.pi
 
 def rectArea(base, height):
-	return 1;
+	return base * height
 
 def trapArea(base1, base2, height):
-	return 1;
+	return height * (base1 + base2) / 2.0
 
 def triArea(base,height):
-	return 1;
-    
-	
+    return base * height / 2.0
+
+def sqArea(sideLength):
+    return sideLength**2.0
+
+""" tests for previously written functions"""
 class MyTest(unittest.TestCase):
     def testCircleArea(self):
-    
+        self.assertEqual(circleArea(5), 25*math.pi, 'testCircleArea')
     def testRectArea(self):
-    
+        self.assertEqual(rectArea(4,5), 4*5, 'testRectArea')
     def testTrapArea(self):
-    
+        self.assertEqual(trapArea(4,5,6), 6 * (4 + 5) / 2, 'testTrapArea')
     def testTriArea(self):
-    
+        self.assertEqual(triArea(3,3), 4.5, 'testTriArea')
+    def testSqArea(self):
+        self.assertEqual(sqArea(2), 4, 'testSqArea')
